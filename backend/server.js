d9643b49cb50b8ed486506a5cf126cf3c9225846
Michael D'Argenio <mdargenio14@gmail.com>
2023-07-12 14:37:11 -0400

    clean up

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,16 @@ const app = express();
 const cors = require("cors");
 
 require("dotenv").config({ path: "./.env" });
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(require("./routes/record"));
 
-// Get MongoDB driver connection
 const dbo = require("./db/conn");
  
 app.listen(port, () => {
-  // Perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
  
