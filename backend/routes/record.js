0fb62e5693dc70307236639d46ce7272063875ef
Michael D'Argenio <mdargenio14@gmail.com>
2023-07-14 19:30:31 -0400

    add update req and test

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -4,11 +4,28 @@ const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
+// Add recipe
 recordRouter.route("/record/add").post(async (req, res) => {
   let db_connect = dbo.getDb();
   try {
     const result = await db_connect.collection("recipes").insertOne(req.body);
     res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Update recipes
+recordRouter.route("/record/update/:id").put(async (req, res) => {
+  const { id } = req.params;
+  let db_connect = dbo.getDb();
+  try {
+    const { _id, ...updatedFields } = req.body;
+    const result = await db_connect
+      .collection("recipes")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updatedFields });
+    res.status(201).json({ updateId: id });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
--- backend/test/create.test.js
+++ backend/test/create.test.js
@@ -16,30 +16,53 @@ describe("Record API", () => {
   after(async () => {
     await dbo.closeConnection();
   });
+  describe.skip("POST /record/add", () => {
+    it("Add a recipe", async () => {
+      const recordData = {
+        name: "Bucatini all'Amatriciana",
+        servings: 4,
+        ingredients: [
+          { name: "extra-virgin olive oil", quantity: 2, unit: "Tbsp" },
+          { name: "guanciale", quantity: 4, unit: "Oz" },
+          { name: "crushed red pepper flakes", quantity: 0.5, unit: "tsp" },
+          { name: "ground black pepper", quantity: 0.5, unit: "tsp" },
+          { name: "minced onion", quantity: 0.75, unit: "cup" },
+          { name: "minced garlic", quantity: 2, unit: "cloves" },
+          {
+            name: "crushed peeled tomatoes",
+            quantity: 1,
+            unit: "28-Oz. can",
+          },
+          { name: "Kosher salt", quantity: "", unit: "" },
+          { name: "dried bucatini", quantity: 12, unit: "Oz" },
+          { name: "finely grated Pecorino", quantity: 1 / 4, unit: "cup" },
+        ],
+        time: [{ time: 45, unit: "minutes" }],
+        instructions: "Cook",
+      };
 
-  it("should create a new record", async () => {
-    const recordData = {
-      name: "Bucatini all'Amatriciana",
-      servings: 4,
-      ingredients: [
-        { name: "extra-virgin olive oil", quantity: 2, unit: "Tbsp" },
-        { name: "guanciale", quantity: 4, unit: "Oz" },
-        { name: "crushed red pepper flakes", quantity: 0.5, unit: "tsp" },
-        { name: "ground black pepper", quantity: 0.5, unit: "tsp" },
-        { name: "minced onion", quantity: 0.75, unit: "cup" },
-        { name: "minced garlic", quantity: 2, unit: "cloves" },
-        { name: "crushed peeled tomatoes", quantity: 1, unit: "28-Oz. can" },
-        { name: "Kosher salt", quantity: "", unit: "" },
-        { name: "dried bucatini", quantity: 12, unit: "Oz" },
-        { name: "finely grated Pecorino", quantity: 1 / 4, unit: "cup" },
-      ],
-      time: [{ time: 45, unit: "minutes" }],
-      instructions: "Cook",
-    };
+      const response = await request(app)
+        .post("/record/add")
+        .send(recordData)
+        .expect(201);
+    });
+  });
+  describe.skip("POST /record/update/:id", () => {
+    it("Update a recipe", async () => {
+      const updatedRecipe = {
+        _id: "64b1d7acecd5526f06acd846",
+        name: "I'm updated",
+        servings: 3,
+        ingredients: ["spaghetti", "tomatoes", "guanciale"],
+        time: ["25 minutes", "medium"],
+        instructions:
+          "Cook the spaghetti, sauté the guanciale and tomatoes, mix together.",
+      };
 
-    const response = await request(app)
-      .post("/record/add")
-      .send(recordData)
-      .expect(201);
+      const updateResponse = await request(app)
+        .put(`/record/update/${updatedRecipe._id}`)
+        .send(updatedRecipe)
+        .expect(201);
+    });
   });
 });
