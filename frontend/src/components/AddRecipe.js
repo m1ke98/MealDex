11bbaef5aee734d57182523f5e6efd87210d5016
Michael D'Argenio <mdargenio14@gmail.com>
2023-07-19 17:31:03 -0400

    add server connection (not working yet)

--- frontend/src/components/AddRecipe.js
+++ frontend/src/components/AddRecipe.js
@@ -1,23 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 export default function AddRecipe() {
-  let ingredients;
-  const [form, setForm] = useState({
-    name: "",
-    servings: "",
-    ingredients: [],
-    time: "",
-    instructions: "",
-  });
-  //   function updateForm(value) {
-  //     return setForm((prev) => {
-  //       return { ...prev, ...value };
-  //     });
-  //   }
-
   function createIngredientList(ingredients) {
     const ingredientsArray = ingredients.split(",").map((item) => item.trim());
     const parsedArray = ingredientsArray.map((ingredient) => {
@@ -32,27 +18,45 @@ export default function AddRecipe() {
     return parsedArray;
   }
 
-  async function submit(e) {
-    e.preventDefault();
-    const newRecipe = { ...form };
-    await fetch("http://localhost:5000/record/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newRecipe),
-    }).catch((error) => {
-      window.alert(error);
-      return;
-    });
-    setForm({
-      name: "",
-      servings: "",
-      ingredients: createIngredientList(ingredients),
-      time: "",
-      instructions: "",
-    });
-  }
+  const submit = async () => {
+    const recipeName = document.getElementById("recipe-name").value;
+    const servings = document.getElementById("servings").value;
+    const time = document.getElementById("time").value;
+    const ingredients = createIngredientList(
+      document.getElementById("ingredients").value
+    );
+    const instructions = document.getElementById("instructions").value;
+
+    const data = {
+      name: recipeName,
+      servings: servings,
+      ingredients: ingredients,
+      time: time,
+      instructions: instructions,
+    };
+
+    console.log(data);
+
+    try {
+      const response = await fetch("http://localhost:5000/record/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const responseData = await response.json();
+      if (response.ok) {
+        console.log("Recipe added successfully!");
+        console.log("Response:", responseData); // Log the response data
+      } else {
+        console.error("Failed to add recipe");
+        console.log("Response:", responseData); // Log the response data
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
     <Box
@@ -66,53 +70,48 @@ export default function AddRecipe() {
       <div>
         <TextField
           required
-          id="standard-textarea"
+          id="recipe-name"
           label="Recipe Name"
           placeholder="Chicken Noodle Soup"
           variant="standard"
-          //   onChange={(e) => updateForm({ name: e.target.value })}
         />
         <TextField
           required
-          id="standard-textarea"
+          id="servings"
           label="Servings"
           placeholder="6"
           variant="standard"
-          //   onChange={(e) => updateForm({ servings: e.target.value })}
         />
         <TextField
           required
-          id="standard-textarea"
+          id="time"
           label="Time"
           placeholder="40 minutes"
           variant="standard"
-          //   onChange={(e) => updateForm({ time: e.target.value })}
         />
       </div>
       <div>
         <TextField
           required
-          id="standard-textarea"
-          label="Ingredient List"
+          id="ingredients"
+          label="Ingredients"
           placeholder="1 tbsp butter, 0.5 cup onion, 1/2 cup celery, 4x14.5oz can chicken broth, 1x14.5oz can vegetable broth, 0.5 lb chicken breast, 1.5 cups egg noodles, 1 cup carrots, 0.5 tsp basil, 0.5 tsp oregano"
           multiline
           variant="standard"
-          //   onChange={(e) => updateForm({ ingredients: e.target.value })}
         />
       </div>
       <div>
         <TextField
           required
-          id="standard-textarea"
+          id="instructions"
           label="Instructions"
           placeholder="Melt butter in a large pot over medium heat. Add onion and celery and cook until just tender, about 5 minutes. Add chicken broth, vegetable broth, chicken, egg noodles, carrots, basil, oregano, salt, and pepper. Stir to combine and bring to a boil. Reduce heat and simmer for 20 minutes."
           multiline
           variant="standard"
-          //   onChange={(e) => updateForm({ instructions: e.target.value })}
         />
       </div>
       <div>
-        <Button variant="contained" disableElevation onClick={submit()}>
+        <Button variant="contained" disableElevation onClick={submit}>
           Submit Recipe
         </Button>
       </div>
